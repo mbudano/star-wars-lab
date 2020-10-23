af938c7cf9e947e0580f266cc0bceadc53880278
Matias Nahuel Budano <budano@ar.ibm.com>
2020-10-23 12:20:06 -0300

    nerdearla production

--- server/server.js
+++ server/server.js
@@ -1,21 +1,19 @@
 const appName = require('./../package').name;
 const http = require('http');
 const express = require('express');
-const log4js = require('log4js');
 const localConfig = require('./config/local.json');
 
-const logger = log4js.getLogger(appName);
-logger.level = process.env.LOG_LEVEL || 'info'
 const app = express();
 const server = http.createServer(app);
 
-app.use(log4js.connectLogger(logger, { level: logger.level }));
+app.use(express.static(__dirname + 'public'));
+app.use('/', express.static(__dirname + '/public'));
 require('./routers/index')(app, server);
 
 
 const port = process.env.PORT || localConfig.port;
 server.listen(port, function(){
-  logger.info(`Server Running on Port:` + port);
+  console.log(`Server Running on Port:` + port);
 });
 
 module.exports = server;
